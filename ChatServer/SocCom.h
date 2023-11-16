9dee2b10f074ef045d364d192cd9cb47e2cddaea
LEE-SUNGRO <sungro0830@gmail.com>
2023-11-16 16:50:01 +0900

    성로 서버

---
+++ ChatServer/SocCom.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+// CSocCom 명령 대상
+// 통신용 소켓
+#define UM_RESEIVE WM_USER+2
+
+class CSocCom : public CSocket
+{
+public:
+	CSocCom();
+	virtual ~CSocCom();
+
+	HWND m_hWnd;                             // 메인 윈도우 핸들
+	void CSocCom::Init(HWND hWnd);           // 소켓 클래스와 메인 윈도우를 연결
+	virtual void OnReceive(int nErrorCode);
+};
+
+
